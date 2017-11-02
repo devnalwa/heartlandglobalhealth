990cb5808bafe6fa3aad2d76e4d86d4df99e6b84
Abyaya Lamsal <am.lamsal@gmail.com>
2017-11-02 14:05:17 +0000

    Fixed the devise_password issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
   validates :email,  presence: true, length: { maximum: 255 },
                      format: { with: EMAIL_REGEX },
                      uniqueness: { case_sensitive: false }
-  has_secure_password
   validates :password, presence: true
 
   def self.search(query)
