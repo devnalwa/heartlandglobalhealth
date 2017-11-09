589d8cd250944d32d1f32377e3925f87c61c8f3c
Abyaya Lamsal <am.lamsal@gmail.com>
2017-11-09 15:12:18 +0000

    removed unnecessary methods

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,3 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters
-
-  protected
-
-#Adding required fields for User
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :school])
-  end
 end
