535e7a6d2fa813dbdeb159d283a867ac34f563cd
Andrew Mack <andrewrmack@gmail.com>
2017-12-03 18:55:53 -0600

    Fix broken tests

--- spec/features/logins_spec.rb
+++ spec/features/logins_spec.rb
@@ -1,13 +1,15 @@
-describe "the signin process", type: :feature do
+require 'rails_helper'
+
+RSpec.feature "Logins", type: :feature do
   before :each do
-   @user = create(:user)
+    @user = create(:user)
   end
 
   it "signs me in" do
     visit user_session_path
     fill_in 'Email', with: @user.email
     fill_in 'Password', with: @user.password
-    click_button 'Sign in'
-    expect(page).to have_content 'Success'
+    click_button 'Log in'
+    expect(page).to have_content 'Log out'
   end
 end
