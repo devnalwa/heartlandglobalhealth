2751ab36bd74e103550882ebfeeb4b5f072a0f03
Nikunj Agrawal <agrawaln@grinnell.edu>
2018-03-05 21:12:27 +0000

    Added routes and an authenticated user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,16 +5,16 @@ class UsersController < ApplicationController
   end
 
   def new
-    # @user = User.new
+    @user = User.new
   end
 
   def create
-    # @user = User.new(user_params)
-    # if @user.save
-    #   redirect_to @user
-    # else
-    #   render 'new'
-    # end
+     @user = User.new(user_params)
+     if @user.save
+       redirect_to @user
+     else
+       render 'new'
+     end
   end
 
   def members
