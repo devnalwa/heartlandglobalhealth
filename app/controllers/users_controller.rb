4fbe21ca670fb70b65057ea1028eb134658cb5a5
JR Lee <leejeung@grinnell.edu>
2018-04-02 19:13:40 +0000

    Modify profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,6 +32,15 @@ class UsersController < ApplicationController
       @users = User.all
     end
   end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
 
   private
   def user_params
